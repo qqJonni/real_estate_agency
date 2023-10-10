5a95f91b732cb393921ebd1a3e693fe8716adc1f
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-10 10:26:45 +0500

    add owner_pure_phone

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town',)
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone']
     list_editable = ['new_building']
     list_filter = ['new_building']
 
